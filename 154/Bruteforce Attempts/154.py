eb57d3e6874d4bb4bbe812bea66823617bd30cae
Robert-App-Dev <robert01.appdev@gmail.com>
2022-03-08 15:00:40 +0000

    154 WIP

---
+++ 154/Bruteforce Attempts/154.py
@@ -0,0 +1,20 @@
+def add_to_dict(dictionary: dict, key, value):
+    if key in dictionary.keys():
+        dictionary[key] += value
+    else:
+        dictionary[key] = value
+
+
+coefs = {(0, 0, 0): 1}
+
+for p in range(1, 200001):
+    new_coefs = {}
+    for c in coefs.keys():
+        add_to_dict(new_coefs, (c[0]+1, c[1], c[2]), coefs[c])
+        add_to_dict(new_coefs, (c[0], c[1]+1, c[2]), coefs[c])
+        add_to_dict(new_coefs, (c[0], c[1], c[2]+1), coefs[c])
+        print(new_coefs)
+    coefs = new_coefs
+    print(p, coefs)
+    if p == 3:
+        break
